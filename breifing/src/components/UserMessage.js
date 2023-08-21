15b4e22644a51b888443f78ff289846c7abdd47d
Dawon00 <dawon000203@gmail.com>
2023-08-21 16:21:26 +0900

    [FE-07] 유저 채팅 컴포넌트 ui 구현

--- breifing/src/components/UserMessage.js
+++ breifing/src/components/UserMessage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as Logo } from "../../src/assets/images/logo.svg";
+import { ReactComponent as You } from "../../src/assets/images/you.svg";
 
 // params 예시
 
@@ -9,7 +9,7 @@ import { ReactComponent as Logo } from "../../src/assets/images/logo.svg";
 
 // const time = "2023/08/21T13:00:00";
 
-function BotMessage({ content, time }) {
+function UserMessage({ content, time }) {
     function formatTime(timeString) {
         const [datePart, timePart] = timeString.split("T");
         const [year, month, day] = datePart.split("/");
@@ -29,12 +29,7 @@ function BotMessage({ content, time }) {
 
     return (
         <div className="flex py-2">
-            <div className="chat-profile-wrap px-3">
-                <div className=" w-9 h-9 rounded-full border-2 border-primaryTextColor flex items-center justify-center">
-                    <Logo className="w-4 h-4" />
-                </div>
-            </div>
-            <div className="message-box-wrap pr-5">
+            <div className="message-box-wrap pl-5">
                 <div className="message-box text-primaryTextColor bg-white rounded-lg p-4">
                     <div className="text-base">{content}</div>
                     <div className="flex justify-end text-xs text-secondTextColor">
@@ -42,8 +37,13 @@ function BotMessage({ content, time }) {
                     </div>
                 </div>
             </div>
+            <div className="chat-profile-wrap px-3">
+                <div className=" w-9 h-9 rounded-full border-2 border-primaryTextColor flex items-center justify-center">
+                    <You className="w-5 h-5" />
+                </div>
+            </div>
         </div>
     );
 }
 
-export default BotMessage;
+export default UserMessage;
--- breifing/src/pages/Storage.js
+++ breifing/src/pages/Storage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BotMessage from "../components/BotMessage";
+import UserMessage from "../components/UserMessage";
 
 const Storage = () => {
     return <div className=" bg-secondBgColor w-full h-screen"></div>;
