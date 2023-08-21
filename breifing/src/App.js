70180ff25360f87abe1dc2d9d26b38fc8fbb68d9
juhui88 <sand8594@hufs.ac.kr>
2023-08-21 14:27:30 +0900

    fix routes

--- breifing/src/App.js
+++ breifing/src/App.js
@@ -1,18 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import BreifChat from "./pages/BreifChat";
 import Storage from "./pages/Storage";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/breifChat" element={<BreifChat />} />
-          <Route path="/storage" element={<Storage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/breifChat" element={<BreifChat />} />
+        <Route path="/storage" element={<Storage />} />
+      </Routes>
     </div>
   );
 }
--- breifing/src/pages/BreifChat.js
+++ breifing/src/pages/BreifChat.js
@@ -1,11 +1,7 @@
-!import React from 'react';
+import React from "react";
 
 const BreifChat = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div></div>;
 };
 
 export default BreifChat;
