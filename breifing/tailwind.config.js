c52fe4659c15b2c568213ffdde8040f7f43d69b7
Dawon00 <dawon000203@gmail.com>
2023-08-21 13:06:30 +0900

    daisy ui setup

--- breifing/tailwind.config.js
+++ breifing/tailwind.config.js
@@ -1,18 +1,18 @@
 //tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primaryBgColor: "#4686CD",
-        primaryTextColor: "#134D80",
-        secondBgColor: "#F7F7F7",
-        secondTextColor: "#B6B6B6",
-      },
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primaryBgColor: "#4686CD",
+                primaryTextColor: "#134D80",
+                secondBgColor: "#F7F7F7",
+                secondTextColor: "#B6B6B6",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [require("daisyui")],
 };
