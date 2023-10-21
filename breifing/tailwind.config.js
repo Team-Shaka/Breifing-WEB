73950c2b8b8544976bed4ca4b4698a8b436be1b2
Dawon00 <dawon000203@gmail.com>
2023-10-21 17:17:41 +0900

    반응형 스크린 단위 설정

--- breifing/tailwind.config.js
+++ breifing/tailwind.config.js
@@ -1,57 +1,62 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        "bottom-sheet-up": {
-          "0%": { transform: "translateY(420px)" },
-          "100%": { transform: "translateY(0)" },
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            keyframes: {
+                "bottom-sheet-up": {
+                    "0%": { transform: "translateY(420px)" },
+                    "100%": { transform: "translateY(0)" },
+                },
+                "bottom-sheet-down": {
+                    "0%": { transform: "translateY(-80px)" },
+                    "100%": { transform: "translateY(0)" },
+                },
+            },
+            fontSize: {
+                xs: [
+                    "10px",
+                    {
+                        lineHeight: "normal",
+                        fontWeight: "400",
+                    },
+                ],
+                sm: [
+                    "13px",
+                    {
+                        lineHeight: "normal",
+                        fontWeight: "400",
+                    },
+                ],
+                base: [
+                    "15px",
+                    {
+                        lineHeight: "normal",
+                        fontWeight: "400",
+                    },
+                ],
+                lg: [
+                    "24px",
+                    {
+                        lineHeight: "normal",
+                        fontWeight: "400",
+                    },
+                ],
+            },
+            colors: {
+                primaryBgColor: "#4686CD",
+                primaryTextColor: "#134D80",
+                secondBgColor: "#F7F7F7",
+                secondTextColor: "#B6B6B6",
+            },
         },
-        "bottom-sheet-down": {
-          "0%": { transform: "translateY(-80px)" },
-          "100%": { transform: "translateY(0)" },
+        screens: {
+            sm: { min: "390px", max: "819px" },
+            md: { min: "820px", max: "1023px" },
+            lg: { min: "1024px" },
         },
-      },
-      fontSize: {
-        xs: [
-          "10px",
-          {
-            lineHeight: "normal",
-            fontWeight: "400",
-          },
-        ],
-        sm: [
-          "13px",
-          {
-            lineHeight: "normal",
-            fontWeight: "400",
-          },
-        ],
-        base: [
-          "15px",
-          {
-            lineHeight: "normal",
-            fontWeight: "400",
-          },
-        ],
-        lg: [
-          "24px",
-          {
-            lineHeight: "normal",
-            fontWeight: "400",
-          },
-        ],
-      },
-      colors: {
-        primaryBgColor: "#4686CD",
-        primaryTextColor: "#134D80",
-        secondBgColor: "#F7F7F7",
-        secondTextColor: "#B6B6B6",
-      },
     },
-  },
-  plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
 };
