87b029fd9b7a77ee3b53aacbe5329f7baf40822e
Dawon00 <dawon000203@gmail.com>
2023-11-22 15:58:12 +0900

    feat: Introduce Section 1 컴포넌트 구현

--- breifing/src/pages/Home.js
+++ breifing/src/pages/Home.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import Header from "../components/homeComps/Header";
 import Footer from "../components/homeComps/Footer";
-import BriefingCardList from '../components/homeComps/BriefingCardList';
+import BriefingCardList from "../components/homeComps/BriefingCardList";
+import IntroduceSection1 from "../components/homeComps/IntroduceSection1";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <BriefingCardList />
+            <IntroduceSection1 />
             <Footer />
         </div>
     );
