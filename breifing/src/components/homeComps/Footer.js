e0555a79d37f710bfba7fa156e19984138c3dacb
juhui88 <sand8594@naver.com>
2023-11-23 13:55:00 +0900

    [FE-36] fix: Header, Footer 반응형

--- breifing/src/components/homeComps/Footer.js
+++ breifing/src/components/homeComps/Footer.js
@@ -4,14 +4,14 @@ import { PiCopyrightBold } from "react-icons/pi";
 
 const Footer = () => {
     return (
-        <div className='flex justify-between bg-primaryBgColor px-20 py-3 text-white'>
+        <div className='flex justify-between bg-primaryBgColor md:px-20 p-3 text-white text-[10px] md:text-[17px]'>
             <div className='flex items-center space-x-1'>
                 <PiCopyrightBold color='white' />
                 <span>
                     2023 TEAM Shaka - Briefing
                 </span>
             </div>
-            <div className='space-x-5'>
+            <div className='md:space-x-5 space-x-10'>
                 <span>X</span>
                 <span>Instagram</span>
             </div>
