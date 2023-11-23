3b5c379a454e2de8999c02e6f602aea5b9cfd009
Dawon00 <dawon000203@gmail.com>
2023-11-23 15:40:47 +0900

    feat: Section component 반응형 화면 구현

--- breifing/src/components/homeComps/InstallSection.js
+++ breifing/src/components/homeComps/InstallSection.js
@@ -5,25 +5,27 @@ import { ReactComponent as InstallSectionImage } from "../../assets/images/insta
 
 function InstallSection() {
     return (
-        <div className="flex justify-center items-center row h-[462px] px-20  bg-primaryBgColor">
+        <div className="flex flex-col lg:flex-row  items-center h-[420px] lg:h-[462px] px-0 lg:px-20  bg-primaryBgColor">
             <div className="flex flex-col  w-1/2 text-left">
-                <div className="text-white text-[45px]">
+                <div className="text-white text-base text-center lg:text-[45px] mt-6">
                     당신의 AI 뉴스 리더,
                 </div>
-                <div className="text-white text-[120px]">Briefing</div>
-                <div className="flex mt-16">
-                    <button className="btn btn-outline rounded-none normal-case w-[175px] h-[57px] text-[20px] font-bold text-white mr-5">
-                        <Apple className=" w-7 h-7" />
-                        App Store
+                <div className="text-white text-center text-[50px] lg:text-[120px]">
+                    Briefing
+                </div>
+                <div className="flex justify-center mt-4 lg:mt-16">
+                    <button className="btn btn-outline rounded-none normal-case w-[130px] lg:w-[175px] h-[35px] lg:h-[57px] text-sm lg:text-[20px] font-bold text-white mr-5">
+                        <Apple className="w-[18px] h-[18px] lg:w-7 lg:h-7" />
+                        <div>App Store</div>
                     </button>
-                    <button className="btn btn-outline rounded-none	normal-case w-[175px] h-[57px] text-[20px] font-bold text-white">
-                        <GooglePlay className="w-7 h-7" />
-                        Google Play
+                    <button className="btn btn-outline rounded-none normal-case w-[130px] lg:w-[175px] h-[35px] lg:h-[57px] text-sm lg:text-[20px] font-bold text-white">
+                        <GooglePlay className="w-[18px] h-[18px] lg:w-7 lg:h-7" />
+                        <div>Google Play</div>
                     </button>
                 </div>
             </div>
-            <div className="flex justify-center mt-auto w-1/2">
-                <InstallSectionImage />
+            <div className="flex justify-center mt-9 lg:mt-auto lg:w-1/2">
+                <InstallSectionImage className="w-[195px] h-[200px] lg:h-[417px]" />
             </div>
         </div>
     );
