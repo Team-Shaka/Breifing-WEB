4ab6da5337f9c7748297b396a7daa3fbd4189c14
juhui88 <sand8594@naver.com>
2024-03-03 21:31:57 +0900

    Fix: Footer css

--- breifing/src/components/aboutComps/Footer.js
+++ breifing/src/components/aboutComps/Footer.js
@@ -8,18 +8,21 @@ import { FaRegCopyright } from "react-icons/fa6";
 
 const Footer = () => {
     return (
-        <div className='flex justify-between bg-whtie p-3  text-primaryBgColor text-base md:text-[18px]'>
-            <div className='flex items-center space-x-1'>
+        <div className='flex justify-center'>
+            <div className='xl:w-[1200px] flex justify-between bg-whtie p-3  text-primaryBgColor text-base md:text-[18px]'>
+                <div className='flex items-center space-x-1'>
 
-                <span className='flex items-center '>
-                    <FaRegCopyright className='mr-2' />
-                    2024 TEAM Shaka - Briefing
-                </span>
-            </div>
-            <div className='md:space-x-5 space-x-10'>
-                <Link to="https://www.instagram.com/briefing_today/">Instagram</Link>
+                    <span className='flex items-center '>
+                        <FaRegCopyright className='mr-2' />
+                        2024 TEAM Shaka - Briefing
+                    </span>
+                </div>
+                <div className='md:space-x-5 space-x-10'>
+                    <Link to="https://www.instagram.com/briefing_today/">Instagram</Link>
+                </div>
             </div>
         </div>
+
     );
 };
 
